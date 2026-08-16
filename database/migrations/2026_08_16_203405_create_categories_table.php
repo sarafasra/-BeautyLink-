bcdf58b4cdf6d86b69a09ad21e926b69d5c683ec
Sarafasra <sarafasraoui75@gmail.com>
2026-08-16 21:34:39 +0100

    creer migration et model de table categorie

--- database/migrations/2026_08_16_203405_create_categories_table.php
+++ database/migrations/2026_08_16_203405_create_categories_table.php
@@ -6,20 +6,22 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
+    /**
+     * Run the migrations.
+     */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('categories', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role', ['client', 'professionnel']);
             $table->timestamps();
         });
     }
 
+    /**
+     * Reverse the migrations.
+     */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('categories');
     }
 };
