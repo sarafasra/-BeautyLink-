b1c3ddfad831fbedd52b1c2672a1a20b8e1f3c62
Sarafasra <sarafasraoui75@gmail.com>
2026-08-16 22:26:14 +0100

    creer migration et model de table service

---
+++ database/migrations/2026_08_16_210141_create_services_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('services', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onDelete('cascade');
+
+            $table->foreignId('category_id')
+                ->constrained('categories')
+                ->onDelete('cascade');
+
+            $table->string('title');
+            $table->text('description');
+            $table->decimal('price', 10, 2);
+            $table->integer('duration');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('services');
+    }
+};
