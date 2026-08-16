2576b9ee67cab07b169c3fad9eff803842e0e64f
Sarafasra <sarafasraoui75@gmail.com>
2026-08-16 22:28:03 +0100

    creer migration et model de table reservation

---
+++ database/migrations/2026_08_16_212718_create_reservations_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('reservations', function (Blueprint $table) {
+            $table->id();
+
+            $table->foreignId('user_id')
+                ->constrained('users')
+                ->onDelete('cascade');
+
+            $table->foreignId('service_id')
+                ->constrained('services')
+                ->onDelete('cascade');
+
+            $table->date('date');
+            $table->time('time');
+
+            $table->enum('status', [
+                'pending',
+                'accepted',
+                'refused',
+                'cancelled'
+            ])->default('pending');
+
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('reservations');
+    }
+};
