aa09045ae0cdeeb4628d7d84efd0bb7627eb95a6
Sarafasra <sarafasraoui75@gmail.com>
2026-08-23 17:03:59 +0100

    Configurer le modèle Review

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -2,9 +2,17 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Review extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'reservation_id',
+        'rating',
+        'comment',
+    ];
 }
