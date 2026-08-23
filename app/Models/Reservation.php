a1f38b859f4bb49829375a6255c4800ad66fd4de
Sarafasra <sarafasraoui75@gmail.com>
2026-08-23 17:03:04 +0100

    Configurer le modèle Reservation

--- app/Models/Reservation.php
+++ app/Models/Reservation.php
@@ -2,9 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Reservation extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'service_id',
+        'date',
+        'time',
+        'status',
+    ];
 }
