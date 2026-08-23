4a45af70e9f43d5d751733e03f1062609695c07c
Sarafasra <sarafasraoui75@gmail.com>
2026-08-23 17:01:16 +0100

    Configurer le modèle Service

--- app/Models/Service.php
+++ app/Models/Service.php
@@ -2,9 +2,19 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'user_id',
+        'category_id',
+        'title',
+        'description',
+        'price',
+        'duration',
+    ];
 }
--- database/migrations/2026_08_16_210141_create_services_table.php
+++ database/migrations/2026_08_16_210141_create_services_table.php
@@ -11,13 +11,9 @@ return new class extends Migration
         Schema::create('services', function (Blueprint $table) {
             $table->id();
 
-            $table->foreignId('user_id')
-                ->constrained('users')
-                ->onDelete('cascade');
+            $table->foreignId('user_id') ->constrained('users')->onDelete('cascade');
 
-            $table->foreignId('category_id')
-                ->constrained('categories')
-                ->onDelete('cascade');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
 
             $table->string('title');
             $table->text('description');
