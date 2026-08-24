9516ba470dd24b0b2ff96f82121498bd090ec961
Sarafasra <sarafasraoui75@gmail.com>
2026-08-24 17:00:56 +0100

    Ajouter les relations du modèle Review

--- app/Models/Review.php
+++ app/Models/Review.php
@@ -15,4 +15,14 @@ class Review extends Model
         'rating',
         'comment',
     ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function reservation()
+    {
+        return $this->belongsTo(Reservation::class);
+    }
 }
