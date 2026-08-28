0a78f3345abb271edb6dc316c0d6dc3defd6ad89
Sarafasra <sarafasraoui75@gmail.com>
2026-08-28 15:53:37 +0100

    Implémenter la connexion des utilisateurs

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -29,4 +29,28 @@ class AuthController extends Controller
             'user' => $user
         ], 201);
     }
+
+
+    public function login(Request $request){
+              $request->validate([
+                'email' => 'required|email',
+                'password' => 'required',
+              ]);
+
+              $user = User::where('email', $request->email)->first();
+
+              if (!$user || !Hash::check($request->password, $user->password)) {
+        return response()->json([
+            'message' => 'Email ou mot de passe incorrect'
+        ], 401);
+    }
+    $token = $user->createToken('auth_token')->plainTextToken;
+            return response()->json([
+                'message' => 'Connexion réussie',
+                'token' => $token,
+                'user' => $user
+            ], 200);
+
+    }
+
 }
