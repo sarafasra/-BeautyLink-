9ea52865add83e8498640a9b528482018b37491e
Sarafasra <sarafasraoui75@gmail.com>
2026-09-07 11:40:19 +0100

    complete authentication et  profile management

--- routes/api.php
+++ routes/api.php
@@ -1,11 +1,23 @@
 <?php
 
+use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProfileController;
 
 Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
-Route::middleware('auth:sanctum')->post('/logout', [AuthController::class, 'logout']);
-Route::get('/profile', [ProfileController::class, 'show']);
+
+Route::middleware('auth:sanctum')->group(function () {
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::get('/user', function (Request $request) {
+        return response()->json([
+            'user' => $request->user()
+        ]);
+    });
+
+    Route::get('/profile', [ProfileController::class, 'show']);
     Route::put('/profile', [ProfileController::class, 'update']);
+});
