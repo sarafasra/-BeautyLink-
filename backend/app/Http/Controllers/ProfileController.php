78d75ec4d7673991dd0c58ce0bab2913ba58c2fd
Sarafasra <sarafasraoui75@gmail.com>
2026-09-07 12:09:40 +0100

    faire la structeur de backend et frontend

---
+++ backend/app/Http/Controllers/ProfileController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+  
+class ProfileController extends Controller
+{
+    public function show(Request $request){
+        return response()->json($request->user());
+    }
+public function update(Request $request)
+{
+    $user = $request->user();
+
+    $request->validate([
+        'name' => 'required|string|max:255',
+        'email' => 'required|email|unique:users,email,' . $user->id,
+    ]);
+
+    $user->update([
+        'name' => $request->name,
+        'email' => $request->email,
+    ]);
+
+    return response()->json([
+        'message' => 'Profile modifié avec succès',
+        'user' => $user
+    ]);
+}
+}
