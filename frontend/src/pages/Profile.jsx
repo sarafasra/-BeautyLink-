a76e7cb00fdf4800e05cd1ea8e9c073557df2f69
Sarafasra <sarafasraoui75@gmail.com>
2026-09-09 20:49:59 +0100

    feat: connecter le profil à l'API

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -85,6 +85,7 @@ function Profile() {
               </div>
             </div>
 
+            {/* Contact Button */}
             <button className="bg-[#9E3B68] hover:bg-[#882F57] text-white font-medium px-8 py-2.5 rounded-xl text-sm shadow-sm transition-all self-stretch md:self-auto text-center">
               Contacter
             </button>
