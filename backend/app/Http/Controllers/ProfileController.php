8c36203673bc2f873b83c787ad7de95dc8d720f4
Sarafasra <sarafasraoui75@gmail.com>
2026-09-09 14:04:07 +0100

    ajouter la route du dashboard

--- backend/app/Http/Controllers/ProfileController.php
+++ backend/app/Http/Controllers/ProfileController.php
@@ -9,23 +9,5 @@ class ProfileController extends Controller
     public function show(Request $request){
         return response()->json($request->user());
     }
-public function update(Request $request)
-{
-    $user = $request->user();
 
-    $request->validate([
-        'name' => 'required|string|max:255',
-        'email' => 'required|email|unique:users,email,' . $user->id,
-    ]);
-
-    $user->update([
-        'name' => $request->name,
-        'email' => $request->email,
-    ]);
-
-    return response()->json([
-        'message' => 'Profile modifié avec succès',
-        'user' => $user
-    ]);
-}
 }
