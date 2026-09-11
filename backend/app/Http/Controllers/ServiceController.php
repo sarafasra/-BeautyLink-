d4587c83fdd666c6c8ec031392119319fcfc035b
Sarafasra <sarafasraoui75@gmail.com>
2026-09-11 12:06:29 +0100

    creer function store

--- backend/app/Http/Controllers/ServiceController.php
+++ backend/app/Http/Controllers/ServiceController.php
@@ -6,5 +6,13 @@ use Illuminate\Http\Request;
 
 class ServiceController extends Controller
 {
-    //
+    public function store(Request $request){
+        $request->validate([
+            'category_id' => 'reqyuired|exists:categories,id',
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'price' => 'required|numeric',
+            'duration' => 'required|integer',
+        ]);
+    }
 }
