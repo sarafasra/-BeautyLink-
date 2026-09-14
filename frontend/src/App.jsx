154de3dd1783365423c57a8442d93ab69b4c362c
Sarafasra <sarafasraoui75@gmail.com>
2026-09-14 11:28:38 +0100

    ajouter le filtrage et la gestion des catégories

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -22,6 +22,7 @@ function App() {
                 />
 <Route path="/profile" element={<Profile />} />
    <Route path="/services"  element={<Services/>}/>
+   <Route path="/services/ajouter" element={<Services />} />
             
             </Routes>
 
--- frontend/src/pages/Services.jsx
+++ frontend/src/pages/Services.jsx
@@ -1,10 +1,16 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import api from "../services/api";
 
 function Services() {
+    const navigate = useNavigate();
+  const isAddPage = window.location.pathname === "/services/ajouter";
+
   const [services, setServices] = useState([]);
   const [editingService, setEditingService] = useState(null);
   const [showAddForm, setShowAddForm] = useState(false);
+  const [message, setMessage] = useState("");
+  const[selectedCategory, setSelectedCategory] = useState("Toutes");
 
   const [newService, setNewService] = useState({
     category_id: 1,
@@ -32,7 +38,7 @@ const addService = async (e) => {
       duration: "",
     });
 
-    setShowAddForm(false);
+    setMessage("Prestation ajoutée avec succès");
 
   } catch (error) {
     console.log(error.response?.data);
@@ -67,6 +73,125 @@ setServices(response.data);
     console.log(error.response?.data);
 }
   };
+
+  const deleteService = async (id) => {
+    try {
+      await api.delete(`/services/${id}`);
+
+      setServices(
+        services.filter((service) => service.id !==id)
+      );
+          setMessage("Prestation supprimée avec succès");
+
+    }catch (error){
+      console.log(error.response?.data);
+      
+    }
+  };
+  if (isAddPage) {
+  return (
+    <div className="min-h-screen bg-pink-50/40 p-8">
+
+      <h1 className="text-2xl font-bold mb-6">
+        Ajouter une prestation
+      </h1>
+      {message && (
+  <div className="bg-green-100 text-green-700 px-4 py-3 rounded-lg mb-4 max-w-xl">
+    ✅ {message}
+  </div>
+)}
+
+
+      <form onSubmit={addService} className="bg-white p-6 rounded-2xl max-w-xl">
+
+        <input
+          type="text"
+          placeholder="Nom de la prestation"
+          value={newService.title}
+          onChange={(e) =>
+            setNewService({
+              ...newService,
+              title: e.target.value,
+            })
+          }
+          className="w-full border rounded-lg p-3 mb-4"
+        />
+
+        <textarea
+          placeholder="Description"
+          value={newService.description}
+          onChange={(e) =>
+            setNewService({
+              ...newService,
+              description: e.target.value,
+            })
+          }
+          className="w-full border rounded-lg p-3 mb-4"
+        />
+        <select
+  value={newService.category_id}
+  onChange={(e) =>
+    setNewService({
+      ...newService,
+      category_id: e.target.value,
+    })
+  }
+  className="w-full border rounded-lg p-3 mb-4"
+>
+  <option value="1">Coiffeure</option>
+  <option value="2">Maquillage</option>
+  <option value="3">Onglerie</option>
+</select>
+
+        <input
+          type="number"
+          placeholder="Prix"
+          value={newService.price}
+          onChange={(e) =>
+            setNewService({
+              ...newService,
+              price: e.target.value,
+            })
+          }
+          className="w-full border rounded-lg p-3 mb-4"
+        />
+
+        <input
+          type="number"
+          placeholder="Durée"
+          value={newService.duration}
+          onChange={(e) =>
+            setNewService({
+              ...newService,
+              duration: e.target.value,
+            })
+          }
+          className="w-full border rounded-lg p-3 mb-4"
+        />
+
+        <div className="flex gap-3">
+
+          <button
+            type="submit"
+            className="bg-[#9A3B68] text-white px-5 py-2 rounded-lg"
+          >
+            Ajouter
+          </button>
+
+          <button
+            type="button"
+            onClick={() => navigate("/services")}
+            className="bg-gray-200 px-5 py-2 rounded-lg"
+          >
+            Annuler
+          </button>
+
+        </div>
+
+      </form>
+    </div>
+  );
+}
   if (editingService)
      {
         console.log("EDITING SERVICE:", editingService);
@@ -105,6 +230,20 @@ setServices(response.data);
           className="w-full border rounded-lg p-3 mb-4"
           placeholder="Description"
         />
+        <select
+  value={editingService.category_id}
+  onChange={(e) =>
+    setEditingService({
+      ...editingService,
+      category_id: e.target.value,
+    })
+  }
+  className="w-full border rounded-lg p-3 mb-4"
+>
+  <option value="1">Coiffeure</option>
+  <option value="2">Maquillage</option>
+  <option value="3">Onglerie</option>
+</select>
 
         <input
           type="number"
@@ -152,6 +291,11 @@ setServices(response.data);
     </div>
   );
 }
+const filteredServices = selectedCategory === "Toutes"
+? services
+: services.filter(
+  (service) => service.category?.name === selectedCategory
+);
 
   return (
     <div className="min-h-screen bg-pink-50/40 p-8">
@@ -239,39 +383,73 @@ setServices(response.data);
           <h1 className="text-2xl font-bold text-gray-900">Mes prestations</h1>
           <p className="text-sm text-gray-500">Gérez vos services, durées et tarifs.</p>
         </div>
-      <button
-  onClick={() => {
-    console.log("AJOUTER CLIQUE");
-    setShowAddForm(true);
-  }}
-  
-        className="bg-[#9A3B68] text-white px-4 py-2 rounded-xl text-sm font-medium hover:bg-[#832e57]">
-          + Ajouter une prestation
-        </button>
+     <button
+  onClick={() => navigate("/services/ajouter")}
+  className="bg-[#9A3B68] text-white px-5 py-3 rounded-xl"
+>
+  + Ajouter une prestation
+</button>
       </div>
-
+  {message && (
+      <div className="bg-green-100 text-green-700 px-4 py-3 rounded-lg mb-6">
+        ✅ {message}
+      </div>
+    )}
       {/* Filter Tabs */}
-      <div className="flex gap-2 mb-8">
-        <button className="px-4 py-1.5 rounded-full text-xs font-medium bg-[#E8A5C2] text-white">
-          Toutes
-        </button>
-        <button className="px-4 py-1.5 rounded-full text-xs font-medium bg-white text-gray-700 border border-gray-200">
-          Coiffure
-        </button>
-        <button className="px-4 py-1.5 rounded-full text-xs font-medium bg-white text-gray-700 border border-gray-200">
-          Coloration
-        </button>
-        <button className="px-4 py-1.5 rounded-full text-xs font-medium bg-white text-gray-700 border border-gray-200">
-          Soins
-        </button>
-      </div>
+<div className="flex gap-2 mb-8">
+
+  <button
+    onClick={() => setSelectedCategory("Toutes")}
+    className={`px-4 py-1.5 rounded-full text-xs font-medium ${
+      selectedCategory === "Toutes"
+        ? "bg-[#E8A5C2] text-white"
+        : "bg-white text-gray-700 border border-gray-200"
+    }`}
+  >
+    Toutes
+  </button>
+
+  <button
+    onClick={() => setSelectedCategory("Coiffeure")}
+    className={`px-4 py-1.5 rounded-full text-xs font-medium ${
+      selectedCategory === "Coiffeure"
+        ? "bg-[#E8A5C2] text-white"
+        : "bg-white text-gray-700 border border-gray-200"
+    }`}
+  >
+    Coiffeure
+  </button>
+
+  <button
+    onClick={() => setSelectedCategory("Maquillage")}
+    className={`px-4 py-1.5 rounded-full text-xs font-medium ${
+      selectedCategory === "Maquillage"
+        ? "bg-[#E8A5C2] text-white"
+        : "bg-white text-gray-700 border border-gray-200"
+    }`}
+  >
+Maquillage  </button>
+
+  <button
+    onClick={() => setSelectedCategory("Onglerie")}
+    className={`px-4 py-1.5 rounded-full text-xs font-medium ${
+      selectedCategory === "Onglerie"
+        ? "bg-[#E8A5C2] text-white"
+        : "bg-white text-gray-700 border border-gray-200"
+    }`}
+  >
+    Onglerie
+  </button>
+
+</div>
+  
 
       {/* Services Grid */}
-      {services.length === 0 ? (
+      {filteredServices.length === 0 ? (
         <p className="text-gray-500">Aucune prestation disponible.</p>
       ) : (
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6">
-          {services.map((service) => (
+          {filteredServices.map((service) => (
             <div
               key={service.id}
               className="bg-white rounded-2xl shadow-sm border border-gray-100 overflow-hidden flex flex-col justify-between"
