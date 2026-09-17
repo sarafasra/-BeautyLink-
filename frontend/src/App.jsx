f8a6db428df755b286f63149af5f7f1f6fbff707
Sarafasra <sarafasraoui75@gmail.com>
2026-09-17 16:14:04 +0100

    modifier dans les routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -36,12 +36,11 @@ function App() {
     path="/client/reservations"
     element={<ClientReservations />}
 />
-            </Routes>
 
                 <Route path="/services" element={<Services />} />
 
                 <Route path="/services/ajouter" element={<Services />} />
-
+    
             </Routes>
         </BrowserRouter>
     );
