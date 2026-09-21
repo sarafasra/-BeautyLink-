4c2de818e0e7d929f6d6bcc7b70f40d0b5e30730
Sarafasra <sarafasraoui75@gmail.com>
2026-09-21 10:45:28 +0000

    complete professional profile

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,7 +8,7 @@ use App\Http\Controllers\ReservationController;
 use App\Http\Controllers\ServiceController;
 
 
-Route::post('/register', [AuthController::class, 'register']);
+Route::post('/register',[AuthController::class,"register"]);
 Route::post('/login', [AuthController::class, 'login']);
 
 Route::middleware('auth:sanctum')->group(function () {
