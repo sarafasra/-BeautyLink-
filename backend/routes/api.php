ef94bd727c8bb1375643808efade53c10a8fd80c
Sarafasra <sarafasraoui75@gmail.com>
2026-09-22 12:24:34 +0000

    Fix profile update route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -22,7 +22,7 @@ Route::middleware('auth:sanctum')->group(function () {
     });
 
     Route::get('/profile', [ProfileController::class, 'show']);
-Route::post('/profile', [ProfileController::class, 'update']);
+Route::put('/profile', [ProfileController::class, 'update']);
     Route::get('/reservations', [ReservationController::class, 'index']);
     Route::get('/professional/reservations', [ReservationController::class, 'professionalReservations']);
 Route::post('/reservations', [ReservationController::class, 'store']);
